2747494b95fb7c90bcfceb317749e277e578115b
emnaDebbabi5 <emna@astraporta.com>
2023-07-11 19:49:13 +0100

    Add ab event name

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Posthog.MixProject do
 
   def project do
     [
-      app: :posthog,
+      app: :ablaevent,
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
